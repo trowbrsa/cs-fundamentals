d1374e7930655e6e52dfec207fd23a5aaf5a5740
Sarah <trowbrsa@gmail.com>
2016-01-08 10:58:26 -0800

    Added new Linked List files from Crystal

--- sorting/mergesort.rb
+++ sorting/mergesort.rb
@@ -1,28 +1,49 @@
 def mergesort(a)
-	# if the array size is 0|1 then the list is considered sorted, return sorted array
-	# split the list in half
-	# merge sort each half
-	# combine the sorted halves
+	if a.length <= 1
+		return a
+	end
+	left, right = split_array(a)
+	left = mergesort(left)
+	right = mergesort(right)
+	return combine(left, right)
 end
 
 def split_array(a)
 	# find the middle
-  	# take = Returns first n elements from the array.
-	# drop = Drops first n elements and returns the rest of the elements.
+	left,right = a.each_slice( (a.size/2.0).round ).to_a
+	# mid = (a.length / 2).round
+	# left = a[0..mid]
+	# right = a[(mid+1)..-1]
 	# return left and right halves of array as separate arrays
-	# [a[0..mid] , a[mid+1..-1]]
-	# return a.take(mid), a.drop(mid)
+	return left, right
 end
 
 # precondition: a and b are sorted
-def combine(a, b)
-	# create a results array
-	
+def combine(left, right)
+	results = []
+	# or, results = Array.new
 	# counters pointing to the index of the smallest elements in each array
-
+	left_index = left[0]
+	right_index = right[0]
 	# check that we have elements to compare
-		# push the smaller element onto the result array
-
+	if left.length && right.length > 0
+		if left_index >= right_index
+			# push the smaller element onto the result array
+			results.push(right_index)
+			left.shift
+			left_index = left[0]
+		else
+			results.push(left_index)
+			right.shift
+			right_index = right[0]
+		end
+		if left.length = 1
+			results.push(left_index)
+		elsif right.length = 1
+			results.push(second_index)
+		end
+	end
+	# only one of these should occur
 	# if there are elements left over in a, move them to result
 	# if there are elements left over in b, move them to result
 
